2ebc9f900411ee600da6304e3a29952c7194e781
Vauxc34 <kaxowy12@gmail.com>
2021-10-05 13:39:55 +0200

    update

--- src/sections/EmailJS.js
+++ src/sections/EmailJS.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
+import styled from 'styled-components'
 
 import './sections_style/ContactUs.css';
+
+const Container_emailbox__1 = styled.div`
+position:relative;
+display:flex;
+height:15%;
+
+`
+
+const Container_labelBox__1 = styled.div`
+position:relative;
+display:flex;
+height:10%;
+
+`
+
+const Container_emailtext__1 = styled.div`
+position:relative;
+flex-direction:column;
+display:flex;
+height:60%;
+
+`
 
 export default function ContactUs() {
 
@@ -18,14 +41,20 @@ export default function ContactUs() {
 
   return (
     <form className="contact-form" onSubmit={sendEmail}>
-      <input type="hidden" name="contact_number" />
+      <Container_labelBox__1>
       <label>Name and username</label>
-      <input type="text" name="user_name"/>
       <label>E-mail Address</label>
+      </Container_labelBox__1>
+      <Container_emailbox__1>
+      <input type="hidden" name="contact_number" />
+      <input type="text" name="user_name"/>
       <input type="email" name="user_email"  />
+      </Container_emailbox__1>
+      <Container_emailtext__1>
       <label>Type your message or question</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
+      </Container_emailtext__1>
     </form>
   );
 }
